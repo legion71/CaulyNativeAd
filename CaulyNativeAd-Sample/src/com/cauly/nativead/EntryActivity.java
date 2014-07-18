3c50053f8d7c1226fe8fe91fe83ef2d90e97aec6
윤창주 <yoonc1@futurestream.co.kr>
2014-07-18 17:26:44 +0900

    - Native CardType 추가

--- CaulyNativeAd-Sample/src/com/cauly/nativead/EntryActivity.java
+++ CaulyNativeAd-Sample/src/com/cauly/nativead/EntryActivity.java
@@ -12,7 +12,7 @@ import android.widget.ListView;
 public class EntryActivity extends Activity   {
 
 	ListView listview; 
-	static final String[] type = {"네이티브 리스트뷰 타입", "네이티브 뷰타입"};
+	static final String[] type = {"네이티브 리스트뷰 타입", "네이티브 뷰타입","네이티브 카드뷰 타입"};
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +25,10 @@ public class EntryActivity extends Activity   {
 			public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
 				if(position==0)
 					startActivity(new Intent(EntryActivity.this, NativeListActivity.class));
-				else
+				else if(position==1)
 					startActivity(new Intent(EntryActivity.this, NativeViewActivity.class));
+				else 
+					startActivity(new Intent(EntryActivity.this, NativeCardActivity.class));
 			}
 		});
     }
